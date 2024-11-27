174c35d12271bc2d69043c94ee481013d36882f8
Aimeric Marchau <marchauaimeric@gmail.com>
2024-11-27 23:31:17 +0100

    delete mariage in model

--- app/models/guest.rb
+++ app/models/guest.rb
@@ -1,6 +1,4 @@
 class Guest < ApplicationRecord
-  belongs_to :mariage
-
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true
--- app/models/mariage.rb
+++
@@ -1,5 +0,0 @@
-class Mariage < ApplicationRecord
-  has_many :guests, dependent: :destroy
-
-  accepts_nested_attributes_for :guests, update_only: true
-end
