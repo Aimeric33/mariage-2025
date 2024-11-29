7142ba30eae6592b78effce9ef1bc66abdac550e
Aimeric Marchau <marchauaimeric@gmail.com>
2024-11-29 23:26:28 +0100

    unvalidate email

--- app/models/guest.rb
+++ app/models/guest.rb
@@ -1,7 +1,6 @@
 class Guest < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :email, presence: true
 
   enum dinner_presence: { dinner_invited: 0, dinner_yes: 1, dinner_no: 2 }
   enum brunch_presence: { brunch_invited: 0, brunch_yes: 1, brunch_no: 2 }
